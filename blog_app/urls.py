aa8def552b27df8d307f6b2f9b43376bc71d438c
Richmond-Andoh <richmondandoh00@gmail.com>
2023-11-05 00:35:28 +0000

    Committing some changes

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('my_blog.urls'))
+    
+    path('profile', user_views.profile, name = 'profile'),
+    path('', include('my_blog.urls')),
+    
 ]
--- my_blog/views.py
+++ my_blog/views.py
@@ -14,48 +14,48 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
-def register(request):
-    if request.method == 'POST':
-        username = request.POST['name']
-        email = request.POST['email']
-        password = request.POST['password']
-        password_confirm = request.POST['password_confirm']
+# def register(request):
+#     if request.method == 'POST':
+#         username = request.POST['name']
+#         email = request.POST['email']
+#         password = request.POST['password']
+#         password_confirm = request.POST['password_confirm']
         
-        if password == password_confirm:
-            if User.objects.filter(username = username).exists():
-                messages.error(request, f'Username {username} already exists')
-                return redirect('blog/register')
-            elif User.email.filter(email = email).exist():
-                messages.error(request, f'Email {email} already exists')
-                return redirect('blog/register')
-            else:
-                user = User.objects.create_user(username=username, email=email, password=password)
-                user.save()
-                return redirect('blog/login')
+#         if password == password_confirm:
+#             if User.objects.filter(username = username).exists():
+#                 messages.error(request, f'Username {username} already exists')
+#                 return redirect('blog/register')
+#             elif User.email.filter(email = email).exist():
+#                 messages.error(request, f'Email {email} already exists')
+#                 return redirect('blog/register')
+#             else:
+#                 user = User.objects.create_user(username=username, email=email, password=password)
+#                 user.save()
+#                 return redirect('blog/login')
             
-        else:
-            messages.error(request, 'Passwords do not match')
-            return redirect('blog/register')
+#         else:
+#             messages.error(request, 'Passwords do not match')
+#             return redirect('blog/register')
         
-    else:
-        return render(request, 'blog/login.html')
+#     else:
+#         return render(request, 'blog/login.html')
     
          
-    return render(request, 'blog/register.html')
+#     return render(request, 'blog/register.html')
 
 
-def login(request):
-    if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
+# def login(request):
+#     if request.method == 'POST':
+#         email = request.POST['email']
+#         password = request.POST['password']
         
-        user = auth.authenticate(email = email, password = password)
-        if user is not None:
-            auth.login(request, user)
-            redirect('blog/home')
+#         user = auth.authenticate(email = email, password = password)
+#         if user is not None:
+#             auth.login(request, user)
+#             redirect('blog/home')
             
-        else:
-            messages.error('Credentials are not captured in our database')
-            redirect('blog/login')
+#         else:
+#             messages.error('Credentials are not captured in our database')
+#             redirect('blog/login')
             
-    return render(request, 'blog/login.html')
+#     return render(request, 'blog/login.html')
